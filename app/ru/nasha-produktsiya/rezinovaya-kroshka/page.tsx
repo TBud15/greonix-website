bf93df61512d66efcf05b83bfa51b1634b752a48
Tymur Budahov <pro1shinov.tima@gmail.com>
2025-11-03 21:58:03 -0500

    tempalte of product page added

--- app/ru/nasha-produktsiya/rezinovaya-kroshka/page.tsx
+++ app/ru/nasha-produktsiya/rezinovaya-kroshka/page.tsx
@@ -1,7 +1,124 @@
+// Server component for good SEO
+// any client components should be imported here to save SEO optimization of this page
+
+import Image from "next/image";
+
 export default function Page() {
   return (
-    <div className="flex justify-center">
-      <p>Наша продукция / Резиновая Крошка</p>
+    <div className="ml-80 font-inter">
+      <p className="text-gray-400">
+        Наша продукция /{" "}
+        <span className="text-green-400">Резиновая Крошка</span>
+      </p>
+
+      {/* Start of page product div */}
+      <div className="flex gap-15 mt-5">
+        {/* Left side photo */}
+        <div>
+          <Image
+            src="/CrumbRubberPage/Product.png"
+            width={550}
+            height={550}
+            alt="Product"
+          />
+        </div>
+
+        <div>
+          {/* Produt info right side */}
+          <div>
+            <h1 className="text-4xl">Резиновая Крошка Greonix</h1>
+
+            {/* Crumb rubber size */}
+            <div className="flex items-center mt-2 ml-0.5">
+              <Image
+                src="/CrumbRubberPage/small-vertical-line.svg"
+                width={3}
+                height={1}
+                alt="Small vertical line"
+                className="mt-2"
+                unoptimized
+              />
+
+              {/* Size variable */}
+              <p className="text-xl mt-1.5 ml-5">0.55 мм</p>
+            </div>
+
+            <hr className="my-6 w-145 border-t border-gray-400/70" />
+
+            {/* Product description paragraph */}
+            {/* First paragraph */}
+            <div className="w-145">
+              <p>
+                Vitamin C, also known as ascorbic acid, is a vital nutrient for
+                various bodily functions, including the maintenance of healthy
+                skin, blood vessels, bones, and cartilage. It also helps with
+                wound healing and acts as an antioxidant.
+              </p>
+
+              {/* Second paragraph */}
+              <p className="mt-5">
+                Vitamin C, also known as ascorbic acid, is a vital nutrient for
+                various bodily functions, including the maintenance of healthy
+                skin, blood vessels, bones, and cartilage. It also helps with
+                wound healing and acts as an antioxidant. Vitamin C, also known
+                as ascorbic acid, is a vital nutrient for various bodily
+                functions, including the maintenance of healthy skin, blood
+                vessels, bones, and cartilage. It also helps with wound healing
+                and acts as an antioxidant. Read more...
+              </p>
+            </div>
+
+            {/* Divider line */}
+            <hr className="my-6 w-145 border-t border-gray-400/70" />
+
+            {/* Fraction section */}
+            <div className="flex items-center gap-7">
+              <div>
+                <p className="text-xl">Фракция</p>
+                <p className="text-gray-400 mt-2">0.55мм</p>
+              </div>
+
+              {/* Fraction photos */}
+              {/* Fraction */}
+              <div className="p-4 border border-[#4CAF4F] rounded-xl border-1">
+                <Image
+                  src="/CrumbRubberPage/ex-1.png"
+                  width={60}
+                  height={60}
+                  alt="Orange"
+                />
+              </div>
+
+              {/* Fraction 2 */}
+              <div className="p-4">
+                <Image
+                  src="/CrumbRubberPage/ex-2.png"
+                  width={60}
+                  height={60}
+                  alt="Orange"
+                />
+              </div>
+
+              <div className="p-4">
+                <Image
+                  src="/CrumbRubberPage/ex-3.png"
+                  width={60}
+                  height={60}
+                  alt="Orange"
+                />
+              </div>
+            </div>
+
+            <hr className="my-6 w-145 border-t border-gray-400/70" />
+          </div>
+          {/* Make order button */}
+          <div>
+            <button className="text-white bg-[#4CAF4F] text-center w-full p-3 rounded-3xl">
+              Оформить Заказ
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
