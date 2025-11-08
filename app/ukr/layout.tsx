0ec14afce60e0a98f7d78a86f1cd14b82611e2ae
Tymur Budahov <pro1shinov.tima@gmail.com>
2025-11-07 23:09:48 -0500

    sitemap and robots.txt added

--- app/ukr/layout.tsx
+++ app/ukr/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ua">
+    <html lang="uk">
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
       >
