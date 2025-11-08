0cf1cafe55d471563d4e00252a84f733cc8b8faf
Tymur Budahov <pro1shinov.tima@gmail.com>
2025-11-07 22:42:04 -0500

    change layout.tsx

--- app/ru/layout.tsx
+++ app/ru/layout.tsx
@@ -1,9 +1,22 @@
-// app/ru/layout.tsx
+// app/layout.tsx
 import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import "../globals.css";
 
 // components import
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
+const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin", "cyrillic"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title:
@@ -18,10 +31,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <Navbar />
-      {children}
-      <Footer />
-    </>
+    <html lang="ru">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
+      >
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
   );
 }
